4d11819862d8351c3569d882b2016a4f67433395
Aleks <bejbi994@gmail.com>
2023-05-17 21:48:05 +0200

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+     <a href="/">Best of Margot</a>
+      </header>
+
+      <main>
+        List of Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
